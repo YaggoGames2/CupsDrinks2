a17e534ecb2c6d0049f372a79e2af6b39753bd9a
YAGGO ROSA DE ALMEIDA SILVA <yaggo.rasilva@senacsp.edu.br>
2025-02-25 17:33:46 -0300

    Aula do dia 25/02/2025

---
+++ Assets/Script/ControladorFase/NewMonoBehaviourScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    internal float TempoRestante;
+
+    public GameObject telaGanhou, telaPerdeuErrou, telaPerdeuTempo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
